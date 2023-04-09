3baa1e370085b1fd4a9b0336ebce62cc51c6ea5e
Masum <masumt999.com>
2023-04-10 00:03:04 +0600

    nav bar and router adder

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import Header from './components/Header/Header';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
-          <h1 className="text-3xl text-purple-600 font-bold underline">
-             Hello world!
-          </h1>
+          <Header></Header>
+          <Outlet></Outlet>
+
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Blog from './components/Blog/Blog'
+import AppliedJobs from './components/AppliedJobs/AppliedJobs'
+import Statistics from './components/Statistics/Statistics'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App></App>,
+    children:[
+      {
+        path: 'blog',
+        element: <Blog></Blog>,
+      },
+      {
+        path:'appliedjob',
+        element: <AppliedJobs></AppliedJobs>,
+      },
+      {
+        path: 'statistics',
+        element: <Statistics></Statistics>,
+      }
+    ]
+  }
+
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <RouterProvider router={router}></RouterProvider>
+  </>,
 )
