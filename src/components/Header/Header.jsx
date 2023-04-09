ee94bdb98fe2095881e02c8992bf7193485adcb6
Masum <masumt999.com>
2023-04-10 01:07:47 +0600

    home section added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,9 @@ import { ShoppingCartIcon } from "@heroicons/react/24/solid";
 
 const Header = () => {
   return (
-    <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-      <div className="relative flex items-center justify-between">
+    <div className="bg-gray-200">
+        <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+      <div className=" flex items-center justify-between">
         <div>
           <Link
             to="/"
@@ -23,7 +24,7 @@ const Header = () => {
           </Link>
         </div>
         <div>
-          <ul className="items-center hidden space-x-8 lg:flex">
+          <ul className="items-center hidden space-x-8 lg:flex font-bold">
             <li>
               <NavLink
               //   to='/'
@@ -174,6 +175,7 @@ const Header = () => {
           )}
         </div> */}
       </div>
+    </div>
     </div>
   );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import businessman from '../../assets/businessman.jpg'
 
 const Home = () => {
     return (
-        <div>
-            <h1>This is Home page</h1>
+        <div className='bg-gray-200'>
+            <div className=' flex  px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 '>
+                <div className='mt-28'>
+                    <h1 className='text-5xl my-5 w-3/5'>Moving you closer to your <span className='text-indigo-500'> dream career.</span></h1>
+                    <h3 className='my-4 w-2/3'>Take control of your job search journey and manage your application process with ease. Our platform allows you to track your applications from start to finish, ensuring that you don't miss any crucial steps. Simplify your job search today and take charge of your future!</h3>
+                    <button className="relative py-3 bg-indigo-500 rounded p-4 text-white font-bold">Get Started</button>
+                </div>
+                <div className='w-full'>
+
+                    <img src={businessman}></img>
+                </div>
+            </div>
         </div>
     );
 };
